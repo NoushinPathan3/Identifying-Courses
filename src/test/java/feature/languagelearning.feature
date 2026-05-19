54ae81cbd06b93d0a295ff7bdd711940970e10d6
Noushin <2487760@cognizant.com>
2026-05-19 12:18:13 +0530

    Added testRunner

---
+++ src/test/java/feature/languagelearning.feature
@@ -0,0 +1,5 @@
+Feature: Language learning
+  Scenario: Extract language learning
+    Given user opens Coursera website
+    When user navigates to Language Learning
+    Then display languages and levels
---
+++ src/test/java/runner/TestRunner.java
@@ -0,0 +1,42 @@
+package runner;
+
+import org.junit.runner.RunWith;
+import io.cucumber.junit.*;
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "Features/coursera.feature",
+        glue = {"stepDefinitions", "hooks"},
+        plugin = {
+                "pretty","html:reports/myreport.html",
+                "summary",
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"
+        },
+        monochrome = true,
+        publish = false
+)
+public class TestRunner {
+
+    static {
+        String timestamp =
+                new SimpleDateFormat("dd-MM-yy HH-mm-ss").format(new Date());
+
+        String baseDir = "test-output/SparkReport " + timestamp;
+
+        new File(baseDir + "/Report").mkdirs();
+        new File(baseDir + "/Screenshots").mkdirs();
+
+        System.setProperty(
+                "extent.reporter.spark.out",
+                baseDir + "/Report/CucumberExtentReport.html"
+        );
+
+        System.setProperty(
+                "screenshots.dir",
+                baseDir + "/Screenshot/"
+        );
+    }
+}
