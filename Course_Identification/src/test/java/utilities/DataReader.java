43b47f91cc00eca9531972f602b0d43d47f1e670
Bushra <Bushrashivani13@gmail.com>
2026-05-21 11:17:30 +0530

    Revert "Excel"
    This reverts commit 25ae6307711db9a5dac254ab91a4f112e8ac6d45.

--- Course_Identification/src/test/java/utilities/DataReader.java
+++ Course_Identification/src/test/java/utilities/DataReader.java
@@ -1,84 +1,55 @@
 package utilities;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-import org.apache.poi.ss.usermodel.*;
+//import org.apache.poi.ss.usermodel.Cell;
+//import org.apache.poi.ss.usermodel.Row;
+
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.*;
+public class DataReader {
+	
+	public static HashMap<String, String> storeValues = new HashMap();
 
-    public class DataReader {
-
-        private static Workbook wb;
-        private static Sheet sheet;
-        private static int rowNum = 0;
-
-        static {
-            try {
-                wb = new XSSFWorkbook();
-                sheet = wb.createSheet("TestResults");
-
-                Row header = sheet.createRow(rowNum++);
-                header.createCell(0).setCellValue("Test Case");
-                header.createCell(1).setCellValue("Result");
-
-            } catch (Exception e) {
-                System.out.println("Error initializing Excel: " + e.getMessage());
-            }
-        }
-
-        //READ DATA
-        public static String getData(String sheetName, int row, int col) {
-
-            try (FileInputStream fis = new FileInputStream("testdata.xlsx");
-                 Workbook wb = WorkbookFactory.create(fis)) {
-
-                Sheet sheet = wb.getSheet(sheetName);
-                return sheet.getRow(row).getCell(col).toString();
-
-            } catch (Exception e) {
-                System.out.println("Error reading Excel: " + e.getMessage());
-                return "";
-            }
-        }
-
-        //WRITE DATA
-        public static synchronized void writeData(String testCase, String result) {
-
-            Row row = sheet.createRow(rowNum++);
-            row.createCell(0).setCellValue(testCase);
-            row.createCell(1).setCellValue(result);
-        }
-
-        public static void saveExcel() {
-
-            try {
-
-                String runPath = RunManager.getRunPath() + "/TestResults.xlsx";
-                String targetPath = "target/TestResults.xlsx";
-                //Save inside Run folder
-                try (FileOutputStream fos = new FileOutputStream(runPath)) {
-                    wb.write(fos);
-                }
-
-                //Copy to target
-                copyFile(runPath, targetPath);
-            } catch (Exception e) {
-                System.out.println("Error writing Excel: " + e.getMessage());
-            }
-        }
-
-        //helper method
-        private static void copyFile(String source, String dest) throws IOException {
-
-            File srcFile = new File(source);
-            File destFile = new File(dest);
-
-            destFile.getParentFile().mkdirs();
-
-            java.nio.file.Files.copy(
-                    srcFile.toPath(),
-                    destFile.toPath(),
-                    java.nio.file.StandardCopyOption.REPLACE_EXISTING
-            );
-        }
-    }
+	public static List<HashMap<String, String>> data(String filepath, String sheetName) throws IOException 
+	 {
+		
+		List<HashMap<String, String>> mydata = new ArrayList<>();
+		
+			FileInputStream file = new FileInputStream(filepath);
+			XSSFWorkbook workbook = new XSSFWorkbook(file);
+			XSSFSheet sheet = workbook.getSheet(sheetName);
+			int totalRows=sheet.getLastRowNum();
+				
+			XSSFRow headerRow=sheet.getRow(0);
+			
+			
+			for (int i = 1; i <= totalRows; i++) 
+				{
+				XSSFRow currentRow = sheet.getRow(i);
+				
+				HashMap<String, String> currentHash = new HashMap<String, String>();
+				
+				for (int j = 0; j < currentRow.getLastCellNum(); j++) 
+					{
+					XSSFCell currentCell = currentRow.getCell(j); 
+					currentHash.put(headerRow.getCell(j).toString(), currentCell.toString());
+				 }
+				mydata.add(currentHash);
+				}
+			file.close();
+			
+		return mydata;
+	}
+}
